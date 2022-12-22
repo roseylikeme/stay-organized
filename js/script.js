3edbbc4f503c54366fe9b00c0c103afbd5e9b8f2
Roselene Gabun <roselenegabun@gmail.com>
2022-12-22 15:15:31 -0800

    Get Selected User's Tasks + Display User
    - Use JQuery to target selected user and display it in its respective
      field.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 "use strict"
+
 // Init Vars Here
 const userDropdown = document.getElementById("userDropdown")
+const userNameField = document.getElementById("userNameField")
+
+let userSelected;
 
 window.addEventListener('load', function(){
     console.log("It loaded!")
     populateDropdown(userDropdown);
+    userDropdown.onchange = viewUserTasks;
 })
 
 function populateDropdown(dropdown) {
@@ -13,8 +18,24 @@ function populateDropdown(dropdown) {
     .then(res => res.json())
     .then(data => {
         for(let user of data){
-            let option = new Option(user["name"], user["id"]);
+            let option = new Option(user.name, user.id);
             dropdown.appendChild(option)
         }
     })
+}
+
+function viewUserTasks() {
+    userSelected = userDropdown.value;
+    fetch(`http://localhost:8083/api/todos/byuser/${userSelected}`)
+    .then(res => res.json())
+    .then(todos => {
+        // If user has no todos then...
+        if ((todos.length < 1) || todos === null) {
+            // TODO: Display no tasks for this user
+        } else {
+            // TODO: Populate Table
+        }
+    })
+    // Display user selected
+    userNameField.innerHTML = $("#userDropdown option:selected").text();
 }
