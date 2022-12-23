8d7c4b1843d5c5353fbb4412fa9aa16933ba6f08
Roselene Gabun <roselenegabun@gmail.com>
2022-12-23 15:57:32 -0800

    Add Navigation Sidebar Feat for Easy Navigation

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ const msg = document.getElementById("msg")
 
 let userSelected;
 
-window.addEventListener('load', function(){
+window.addEventListener('load', function () {
     console.log("It loaded!")
     populateDropdown(userDropdown);
     userDropdown.onchange = viewUserTasks;
@@ -18,13 +18,21 @@ window.addEventListener('load', function(){
 function populateDropdown(dropdown) {
     console.log("This is to populate users names")
     fetch("http://localhost:8083/api/users")
-    .then(res => res.json())
-    .then(data => {
-        for(let user of data){
-            let option = new Option(user.name, user.id);
-            dropdown.appendChild(option)
-        }
-    })
+        .then(res => res.json())
+        .then(data => {
+            for (let user of data) {
+                let option = new Option(user.name, user.id);
+                dropdown.appendChild(option)
+            }
+        })
+}
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
 }
 
 function viewUserTasks() {
@@ -33,37 +41,37 @@ function viewUserTasks() {
     userSelected = userDropdown.value;
 
     fetch(`http://localhost:8083/api/todos/byuser/${userSelected}`)
-    .then(res => res.json())
-    .then(todos => {
-        console.log(todos.length)
-        // If a user has no todos then...
-        if ((todos.length < 1) || todos === null) {
-            todoTable.style.display = "none";
-            msg.innerHTML = "This user has no tasks to do."
-        }
-        // If a user contains a todo then display the todos.
-        else {
-            for (let task of todos) {
-                let row = todoTableBody.insertRow(-1);
-                for (let property in task) {
-                    switch (property) {
-                        // If one of these properties > add to cell
-                        case "description":
-                        case "deadline":
-                        let cell = row.insertCell(-1);
-                        cell.innerHTML = task[property];
-                        break;
-                    }
-                }
-                let anchorCell = row.insertCell(-1);
-                let anchor = document.createElement("a");
-                anchor.href = `todo_details.html?toDoid=${task.id}`;
-                anchor.text = "See Details";
-                anchorCell.appendChild(anchor);
+        .then(res => res.json())
+        .then(todos => {
+            console.log(todos.length)
+            // If a user has no todos then...
+            if ((todos.length < 1) || todos === null) {
+                todoTable.style.display = "none";
+                msg.innerHTML = "This user has no tasks to do."
             }
-            todoTable.style.display = "block";
-        }
-        // Display user selected
-        userNameField.innerHTML = $("#userDropdown option:selected").text();
-    });
+            // If a user contains a todo then display the todos.
+            else {
+                for (let task of todos) {
+                    let row = todoTableBody.insertRow(-1);
+                    for (let property in task) {
+                        switch (property) {
+                            // If one of these properties > add to cell
+                            case "description":
+                            case "deadline":
+                                let cell = row.insertCell(-1);
+                                cell.innerHTML = task[property];
+                                break;
+                        }
+                    }
+                    let anchorCell = row.insertCell(-1);
+                    let anchor = document.createElement("a");
+                    anchor.href = `todo_details.html?toDoid=${task.id}`;
+                    anchor.text = "See Details";
+                    anchorCell.appendChild(anchor);
+                }
+                todoTable.style.display = "block";
+            }
+            // Display user selected
+            userNameField.innerHTML = $("#userDropdown option:selected").text();
+        });
 }
